7d81f520215a95f0ccfd3249eac2ee13e3461be3
N. A. Ronald <nronald@swin.edu.au>
2023-08-08 15:16:34 +1000

    Added intent and parcelable.

--- app/src/main/java/au/edu/swin/sdmd/l06_detailedimages_2023/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l06_detailedimages_2023/DetailActivity.kt
@@ -1,11 +1,27 @@
 package au.edu.swin.sdmd.l06_detailedimages_2023
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val location = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra("location", Location::class.java)
+        } else {
+            intent.getParcelableExtra<Location>("location")
+        }
+
+        location?.let {
+            val vName = findViewById<TextView>(R.id.name)
+            vName.text = location.name
+
+            val vAuthor = findViewById<TextView>(R.id.author)
+            vAuthor.text = location.author
+        }
     }
 }
